3a485155a761dd4f2addce5d648b6b04fc6c7b3f
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-04 00:11:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function getFormvalue() {
 	const fname=document.querySelector('input[name="fname"]').value;
 	const lname=document.querySelector('input[name="lname"]').value;
 	alert(fname+" "+lname);
+	
+	/*	or
+	const form=document.forms["form1"];
+
+	const firstName = form["fname"].value;
+	const lastName = form["lname"].value;
+	alert(firstName+" "+lastName);*/
 }
